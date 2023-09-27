8e8664a043593c4910c3166543d3a3a038b6bce7
OsmanSotskyi <sockiy.o@gmail.com>
2023-09-27 18:30:26 +0100

    new

---
+++ src/test/java/class3/XpathHW.java
@@ -0,0 +1,36 @@
+package class3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class XpathHW {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demoqa.com/text-box");
+        driver.manage().window().maximize();
+
+
+        WebElement FulName = driver.findElement(By.xpath("//input[@id='userName']"));
+        FulName.sendKeys("Osman Sotskyi");
+
+        WebElement email = driver.findElement(By.xpath("//input[@id='userEmail']"));
+        email.sendKeys("sockiy.o@gmail.com");
+
+        WebElement currentAddress = driver.findElement(By.xpath("//textarea[@id='currentAddress']"));
+        currentAddress.sendKeys("flat 1, Armitage Bridge House, Huddersfield, UK");
+
+        WebElement permanentAddress = driver.findElement(By.xpath("//textarea[@id='permanentAddress']"));
+        permanentAddress.sendKeys("flat 1, Armitage Bridge House, Huddersfield, UK");
+
+        driver.findElement(By.xpath("//button[@id='submit']")).click();
+
+
+
+
+
+
+
+    }
+}
